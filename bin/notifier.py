1f3d08ace60b0c0bb467017fdc4e230f52a25f38
Zybyte85 <cade.step01@gmail.com>
2024-09-03 18:15:20 -0600

    Commented out the import statement in the notifyer.py file

--- bin/notifier.py
+++ bin/notifier.py
@@ -1,11 +1,11 @@
-from plyer import notification
-import json
+#from plyer import notification
+#import json
 
 # In the other file, we will make a seperate json file that just has all of the notification times (and possibly data) and then we will push the notifications here.
 
-notification.notify(
-    title="HEADING HERE",
-    message=" DESCRIPTION HERE",
+#notification.notify(
+#    title="HEADING HERE",
+#    message=" DESCRIPTION HERE",
     # displaying time
-    timeout=2,
-)
+#    timeout=2,
+#)
